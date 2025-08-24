5932b8a8032d1655b7a11b242870455f174d19f3
Kissmybullet <128668418+Kissmybullet@users.noreply.github.com>
2025-08-25 02:15:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@
 #..........Just one requests do not remove my credit..........#
 
 import os
-API_ID = 1234567
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = 22403100
+API_HASH = "ccbc3f662735abfa604ef6309ba76e67"
+BOT_TOKEN = "6255221851:AAFSXLwuNUxQ17tnU1TaHF5EJlAeugrJKwQ"
 #Logs
-LOG_CHANNEL = "" 
+LOG_CHANNEL = "-1001586987735" 
 # MongoDB setup
-MONGO_URL = ""
-DB_NAME = ""
+MONGO_URL = "mongodb+srv://Divyanshu:gameno01@cluster0.bcj1u2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = "AnimeDl"
 
 DOWNLOAD_DIR = "./downloads"
 #ADMIN INFO
-ADMIN = 
+ADMIN = 5817124748
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/dfd1842d8a2dcc536a2b7.jpg https://graph.org/file/b55f0baaa7a6fde7c5682.jpg https://graph.org/file/3298ca8910c82f33418a8.jpg https://graph.org/file/4e4935469a7214c734721.jpg https://graph.org/file/fa5f2b241fe77beff8ba0.jpg https://graph.org/file/bcb9969f78ab4a47a483c.jpg https://graph.org/file/a9af161696d82f17b8888.jpg https://graph.org/file/d23b00650c00ce4d9a467.jpg https://graph.org/file/4dc0b3dfaad61fbcf0a49.jpg https://graph.org/file/7088315e9b0b6a2fa7118.jpg https://graph.org/file/9c9911d06e5f2316febb9.jpg https://graph.org/file/d2ee185180469cfd28071.jpg https://graph.org/file/dea04b2d615406aeb0181.jpg https://graph.org/file/98b63d3bb84984a68cc76.jpg").split()
